c6bbc4366eae0771f6afb2fda81403b789e23de2
ch13mob <cn13mob@gmail.com>
2021-04-16 12:22:49 +0300

    Feat: add Animated Text Kit

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
+import 'package:animated_text_kit/animated_text_kit.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_animations/dashatar.dart';
 
 void main() {
   runApp(MyApp());
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blueGrey,
       ),
-      home: MyHomePage(title: 'Flutter Animations'),
+      home: MyHomePage(title: 'Animated Text Kit'),
     );
   }
 }
@@ -35,13 +35,40 @@ class _MyHomePageState extends State<MyHomePage> {
         title: Text(widget.title),
       ),
       body: Center(
-        child: Dashatar(),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          setState(() {});
-        },
-        child: Icon(Icons.play_arrow),
+        child: Padding(
+          padding: EdgeInsets.all(15.0),
+          child: SizedBox(
+            width: double.infinity,
+            child: AnimatedTextKit(
+              animatedTexts: [
+                TypewriterAnimatedText(
+                  'The first rule of Fight Club is: you do not talk about Fight Club.',
+                ),
+                TypewriterAnimatedText(
+                  'The second rule of Fight Club is: you do never talk about Fight Club.',
+                ),
+                TypewriterAnimatedText(
+                  'Third rule of Fight Club: someone yells stop, goes limp, taps out, the fight is over.',
+                ),
+                TypewriterAnimatedText(
+                  'Fourth rule: only two guys to a fight.',
+                ),
+                TypewriterAnimatedText(
+                  'Fifth rule: one fight at a time, fellas.',
+                ),
+                TypewriterAnimatedText(
+                  'Sixth rule: no shirts, no shoes.',
+                ),
+                TypewriterAnimatedText(
+                  'Seventh rule: fights will go on as long as they have to.',
+                ),
+                TypewriterAnimatedText(
+                  'And the eighth and final rule: if this is your first night at Fight Club, you have to fight.',
+                ),
+              ],
+            ),
+          ),
+        ),
       ),
     );
   }
